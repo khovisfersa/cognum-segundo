0c58ea061e90c4cd4ac53a95a3ec22adfbf2d294
khovisfersa <lucaskfsantos@gmail.com>
2023-09-12 15:00:43 -0300

    adding database support on elephantSQL

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 
-const app = express()
+const { Pool } = require('pg');
+const fs = require('fs');
+const app = express();
+require('dotenv').config();
 
-app.listen(3000)
+app.use(cors({
+	origin: '*'
+}));
+
+
+const PORT = process.env.PORT || 3333
+app.listen(PORT)
+
+const pool = new Pool({
+	connectionString: process.env.POSTGRES_URL
+})
+
+app.post("/create_employee", async (req,res)=> {
+    const {id, name, role} = req.body
+    try {
+        // const employee = pool.query('INSERT INTO ') 
+    } catch(err) {
+
+    }
+})
